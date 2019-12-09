a1249ddf8efe0a5dd7f2242ab6680a96a3bf040e
dh.hsu <tony4120523@gmail.com>
2019-12-10 01:18:16 +0800

    add files

---
+++ src/MyPackage/Writable.java
@@ -0,0 +1,5 @@
+package MyPackage;
+
+public interface Writable {
+    void doWrite(String w);
+}
---
+++ src/MyPackage/Writer.java
@@ -0,0 +1,8 @@
+package MyPackage;
+
+abstract class Writer implements Writable {
+    // codes
+
+    // note that modifiers access privileges
+    public void doWrite(String w) {};
+}
