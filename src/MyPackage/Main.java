fc66f9e1838a7523cae63c2a94fc139ee1e60649
dh.hsu <tony4120523@gmail.com>
2019-12-27 18:16:38 +0800

    update Main

--- src/MyPackage/Main.java
+++ src/MyPackage/Main.java
@@ -2,6 +2,7 @@ package MyPackage;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,11 +20,25 @@ public class Main {
 
 
         // 測試 stream.mapToInt() 用法
-        List<String> list = Arrays.asList("Joe", "Paul", "Alice", "Tom");
-        System.out.println (
-            // list.stream().filter(x -> x.length() > 3).mapToInt(x -> x).count()  這邊 String 是不能轉為 int 的
-            list.stream().filter(x -> x.length() > 3).count()  // 要這樣子才對
-        );
+        // List<String> list = Arrays.asList("Joe", "Paul", "Alice", "Tom");
+        // System.out.println (
+        //     // list.stream().filter(x -> x.length() > 3).mapToInt(x -> x).count()  這邊 String 是不能轉為 int 的
+        //     list.stream().filter(x -> x.length() > 3).count()  // 要這樣子才對
+        // );
+
+
+        // 測試 findFirst()，找到符合的就不繼續往下找
+        List<String> str = Arrays.asList("my", "pen", "is", "your", "pen");
+        Predicate<String> test = s -> {
+            int i = 0;
+            boolean result = s.contains("pen");
+            System.out.print(i++ + " : ");
+            return result;
+        };
+        str.stream()
+                .filter(test)
+                .findFirst()
+                .ifPresent(System.out::print);
 
     }
 }
